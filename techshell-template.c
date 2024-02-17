199e3eccefb57539b1b9d5a0ba63e6be4d2fe745
Grant Cooper <69401337+Parzeval2@users.noreply.github.com>
2024-02-17 17:13:27 -0600

    Worky worky

--- techshell-template.c
+++ techshell-template.c
@@ -11,59 +11,144 @@
 #include <sys/types.h>
 #include <wait.h>
 #include <string.h>
+#include <ctype.h>
 
 struct ShellCommand{
     char *command;
     int argument_count;
     char **arguments;
+    char *input_redirection;
+    char *output_redirection;
+    int append_out;
 };
+/**
+ * @brief Display current working directory and return user input
+ *
+ * @return char* user input
+ */
 char* CommandPrompt(){
     //display working directory
-    char* cwd;
-    cwd=(char*)get_current_dir_name();
-
-    printf("%s: ", cwd);
+    char cwd[1024];
+   if(getcwd(cwd, sizeof(cwd)) == NULL){
+       perror("getcwd");
+       return NULL;
+   }
+    printf("%s: $", cwd);
 
     //return user input
-    char *prompt;
-    scanf("%s", prompt);
+    char *prompt = NULL;
+    size_t prompt_size = 0;
+    getline(&prompt, &prompt_size, stdin);
     return prompt;
 
 } // Display current working directory and return user input
 
-struct ShellCommand ParseCommandLine(char* input){
-    struct ShellCommand command;
+/**
+ * @brief Process the user input (As a shell command)
+ *
+ * @param input user input
+ * @return struct ShellCommand
+ */
+struct ShellCommand ParseCommandLine(char* input)
+{
+    struct ShellCommand command = {0};
     command.argument_count=0;
-    command.arguments=(char**)malloc(sizeof(char*)*100);
+    command.arguments=malloc(10 *sizeof(char*));
+    command.append_out=0;
+    command.input_redirection=NULL;
+    command.output_redirection=NULL;
 
     //commands and args
-    command.command = strtok(input, " ");
-    while (command.command!= NULL) {
-        command.arguments[command.argument_count] = command.command;
-        command.argument_count++;
-        command.command = strtok(NULL, " ");
-    }
+    char *token;
+    char *next = input;
+    char *end = input + strlen(input);
 
-    //shell command struct
+    while(next < end){
+        while(isspace((unsigned char)*next)){ //skip spaces
+            next++;
+        }
+        if(*next == '\"'){ //open quotes
+            next++;
+            token = next;
+            while(*next && *next!= '\"'){ //closing quotes
+                next++;
+            }
+        }
+        else{ //regular token
+            token = next;
+            while(*next &&!isspace((unsigned char)*next) && *next!= '\"'){
+                next++;
+            }
+        }
+
+        if (*next){ //null terminator
+            *next++ = '\0';
+        }
+
+
+        if(token < next){
+            if(strcmp(token, "<")==0)
+            {
+                token = strtok(NULL, " \n");
+                command.input_redirection=token;
+            }
+            else if(strcmp(token, ">")==0)
+            {
+                token = strtok(NULL, " \n");
+                command.output_redirection=token;
+                command.append_out=0;
+            }
+            else if(strcmp(token, ">>")==0)
+            {
+                token = strtok(NULL, " \n");
+                command.output_redirection=token;
+                command.append_out=1;
+            }
+            else
+            {
+                command.arguments[command.argument_count++]=strdup(token);
+            }
+        }
+    }
+        if(command.argument_count>0)
+        {
+            command.command=command.arguments[0];
+        }
 
     return command;
-} // Process the user input (As a shell command)
+}
 
+void SetupRedirect(struct ShellCommand *command){
+    if(command->input_redirection){
+        freopen(command->input_redirection, "r", stdin);
+    }
+    if(command->output_redirection){
+        if(command->append_out){
+            freopen(command->output_redirection, "a", stdout);
+        }
+        else{
+            freopen(command->output_redirection, "w", stdout);
+        }
+    }
+}
 void ExecuteCommand(struct ShellCommand command){
     pid_t pid = fork();
     if (pid == 0) {
+        SetupRedirect(&command);
         char* arguments[command.argument_count + 1];
-        arguments[0] = command.command;
         for (int i = 0; i < command.argument_count; i++) {
-            arguments[i + 1] = command.arguments[i];
+            arguments[i] = command.arguments[i];
         }
-        arguments[command.argument_count + 1] = NULL;
+        arguments[command.argument_count] = NULL;
         execvp(command.command, arguments);
-        exit(1);
+        exit(EXIT_SUCCESS);
     }
-    else{
+    else if (pid > 0) {
         int status;
         waitpid(pid, &status, 0);
+    }
+    else{
+        perror("fork");
     }
 }//Execute a shell command
 
@@ -76,6 +161,9 @@ int main() {
     {
         input = CommandPrompt();
         // parse the shellCommand line
+        if (strcmp(input, "exit") == 0) {
+            break;
+        }
         shell_command = ParseCommandLine(input);
         // execute the shellCommand
         ExecuteCommand(shell_command);
